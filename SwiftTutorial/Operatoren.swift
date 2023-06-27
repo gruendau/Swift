bd0494bad61e31f913ec5c9a48154109a16d2270
gruendau <volker.ka@protonmail.com>
2023-06-27 18:52:16 +0200

    function inout

---
+++ SwiftTutorial/Operatoren.swift
@@ -0,0 +1,19 @@
+//
+//  Operatoren.swift
+//  SwiftTutorial
+//
+//  Created by volker on 27.06.23.
+//
+
+import Foundation
+
+struct Operatoren {
+    
+    func increment () {
+//        var a = 1
+//        var b = a++
+//        b++
+//        print(a, b)
+    }
+}
+
--- SwiftTutorial/main.swift
+++ SwiftTutorial/main.swift
@@ -14,7 +14,26 @@ import Foundation
 //Arrays().importantArrayPropertiesAndMethodes()
 //Arrays().allArrayPropertiesAndMethodes()
 
-test_print_vs_dump()
+//test_print_vs_dump()
+
+var x = 1
+
+func erhöhe (_ x: Int) {
+    var x = x
+    x += 1
+    print("in function: \(x)")
+}
+
+erhöhe(x)
+print("out function: \(x)")
+
+func erhöhe_inout (_ x: inout Int) {
+    x += 1
+    print("in function: \(x)")
+}
+
+erhöhe_inout(&x)
+print("out function: \(x)")
 
  
 
